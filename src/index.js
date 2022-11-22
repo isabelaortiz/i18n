c4a3c81fd433602bee68466ae9f34cfdbda8aa23
Isabela Ortiz <i.ortiz@uniandes.edu.co>
2022-11-22 09:14:06 -0500

    localizacion

--- src/index.js
+++ src/index.js
@@ -1,6 +1,34 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { IntlProvider } from 'react-intl';
 
 import JobsList from "./components/jobslist";
+import localeEsMessages from "./locales/es";
+import localeEnMessages from "./locales/en";
 
-ReactDOM.render(<JobsList />, document.getElementById("root"));
+
+var usrlang = navigator.language
+    || navigator.userLanguage;
+console.log(
+    "User's preferred language is: "
+    + usrlang);
+
+const language = () => {
+    if (usrlang == "es") return 'es-ES';
+    else return 'en-EN';
+};
+
+const languageL = () => {
+    if (language() === "es-ES") return localeEsMessages;
+    else return localeEnMessages;
+};
+
+ReactDOM.render(
+    <IntlProvider locale= {language()} messages={languageL()}>
+        <JobsList />
+    </IntlProvider>, document.getElementById("root")
+);
+
+
+
+
